091e2504e944b83f12ef5085a3218af869c625da
Shreenath <Shreenath@DESKTOP-EESGNJA>
2024-03-16 17:49:08 +0530

    selfName

---
+++ DemoAutomation/src/Automation/SelfName.java
@@ -0,0 +1,19 @@
+package Automation;
+
+public class SelfName {
+	
+	public static void main(String[] args) {
+		
+	
+    char ch = '\u0928' ;
+    char ch1= '\u0940';
+    char ch2= '\u0932';
+    char ch3= '\u0946';
+    char ch4= '\u0936';
+    
+    
+    
+    System.out.println(ch+""+ch1+""+ch2+""+ch3+""+ch4);
+
+}
+}
