3b958e824b1625e98f87fa632b0019a1b650cd2c
Shreenath <Shreenath@DESKTOP-EESGNJA>
2024-03-22 22:18:14 +0530

    java program of square cube fourth power

---
+++ DemoAutomation/src/Automation/SquareCubeFourthPower.java
@@ -0,0 +1,17 @@
+package Automation;
+
+public class SquareCubeFourthPower {
+
+	public static void main(String[] args) {
+		
+		
+			
+		
+		double i = 2;
+		
+		System.out.println("Square = "+i*i);
+		System.out.println("Cube = "+i*i*i);
+		System.out.println("Fourth Power = "+i*i*i*i);
+	}
+
+}
