8a320e5246aad4bb428cfa24a3807cb4c12f1bf1
Shreenath <Shreenath@DESKTOP-EESGNJA>
2024-03-30 12:13:03 +0530

    Calendar

---
+++ DemoAutomation/src/automation/Calendar.java
@@ -0,0 +1,104 @@
+package automation;
+
+public class Calendar {
+
+	public static void main(String[] args) {
+
+		int num = 1;
+
+		switch (num) {
+		case 1:
+			System.out.println("January - 31");
+			break;
+		case 2:
+			System.out.println("February -28/29");
+			break;
+		case 3:
+			System.out.println("March - 31");
+			break;
+		case 4:
+			System.out.println("April - 30");
+			break;
+		case 5:
+			System.out.println("May -31");
+			break;
+		case 6:
+			System.out.println("June - 30");
+			break;
+		case 7:
+			System.out.println("July - 31");
+			break;
+		case 8:
+			System.out.println("August -31");
+			break;
+		case 9:
+			System.out.println("September -30");
+			break;
+		case 10:
+			System.out.println("October -31");
+			break;
+		case 11:
+			System.out.println("November -30");
+			break;
+		case 12:
+			System.out.println("December -31");
+			break;
+
+		default:
+			System.out.println("Invalid input");
+
+		}
+
+		if (num == 1) {
+			System.out.println("January - 31");
+		}
+		if (num == 2) {
+			System.out.println("February 28/29");
+
+		}
+		if (num == 3) {
+			System.out.println("March -30");
+
+		}
+		if (num == 4) {
+			System.out.println("April -31");
+
+		}
+		if (num == 5) {
+			System.out.println("May -31");
+
+		}
+		if (num == 6) {
+			System.out.println("June -30");
+
+		}
+		if (num == 7) {
+			System.out.println("July -31");
+
+		}
+		if (num == 8) {
+			System.out.println("August -31");
+
+		}
+		if (num == 9) {
+			System.out.println("September");
+
+		}
+		if (num == 10) {
+			System.out.println("October");
+
+		}
+		if (num == 11) {
+			System.out.println("November - 30");
+
+		}	if (num == 12) {
+			System.out.println("December -31");
+
+		}
+		else {
+			System.out.println("Invalid input");
+		}
+
+	}
+
+}
